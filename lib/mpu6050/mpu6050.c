c559bb4234352c9c469e743dc12d9ebbbd21557e
Henrique O. Santos <202410486@uesb.edu.br>
2025-07-28 15:21:16 -0300

    Refactor MPU6050 library to use internal I2C instance
    Introduced mpu6050_init to set the I2C instance internally, removing the need to pass the I2C port to each function call. Updated mpu6050_reset and mpu6050_read_raw to use the internal I2C instance, simplifying the library and usage.

--- lib/mpu6050/mpu6050.c
+++ lib/mpu6050/mpu6050.c
@@ -1,7 +1,12 @@
 #include "mpu6050.h"
 #include "pico/binary_info.h"
 
+static i2c_inst_t *i2c_port;
 
+void mpu6050_init(i2c_inst_t *I2C_PORT) {
+    i2c_port = I2C_PORT;
+    mpu6050_reset();
+}
 
 void setup_I2C_mpu6050(i2c_inst_t *I2C_PORT, uint I2C_SDA, uint I2C_SCL, uint clock) {
     // INICIALIZAÇÃO DO I2C COM A FREQUÊNCIA DE CLOCK
@@ -14,28 +19,27 @@ void setup_I2C_mpu6050(i2c_inst_t *I2C_PORT, uint I2C_SDA, uint I2C_SCL, uint cl
 
 }
 
-
 // Função para resetar e inicializar o MPU6050
-void mpu6050_reset(i2c_inst_t *I2C_PORT) {
+void mpu6050_reset() {
     // Dois bytes para reset: primeiro o registrador, segundo o dado
     uint8_t buf[] = {0x6B, 0x80};
-    i2c_write_blocking(I2C_PORT, mpu6050_address, buf, 2, false);
+    i2c_write_blocking(i2c_port, mpu6050_address, buf, 2, false);
     sleep_ms(100); // Aguarda reset e estabilização
 
     // Sai do modo sleep (registrador 0x6B, valor 0x00)
     buf[1] = 0x00;
-    i2c_write_blocking(I2C_PORT, mpu6050_address, buf, 2, false);
+    i2c_write_blocking(i2c_port, mpu6050_address, buf, 2, false);
     sleep_ms(10); // Aguarda estabilização após acordar
 }
 
 // Função para ler dados crus do acelerômetro, giroscópio e temperatura
-void mpu6050_read_raw(i2c_inst_t *I2C_PORT,int16_t accel[3], int16_t gyro[3], int16_t *temp) {
+void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
     uint8_t buffer[6];
 
     // Lê aceleração a partir do registrador 0x3B (6 bytes)
     uint8_t val = 0x3B;
-    i2c_write_blocking(I2C_PORT, mpu6050_address, &val, 1, true);
-    i2c_read_blocking(I2C_PORT, mpu6050_address, buffer, 6, false);
+    i2c_write_blocking(i2c_port, mpu6050_address, &val, 1, true);
+    i2c_read_blocking(i2c_port , mpu6050_address, buffer, 6, false);
 
     for (int i = 0; i < 3; i++)
     {
@@ -44,8 +48,8 @@ void mpu6050_read_raw(i2c_inst_t *I2C_PORT,int16_t accel[3], int16_t gyro[3], in
 
     // Lê giroscópio a partir do registrador 0x43 (6 bytes)
     val = 0x43;
-    i2c_write_blocking(I2C_PORT, mpu6050_address, &val, 1, true);
-    i2c_read_blocking(I2C_PORT, mpu6050_address, buffer, 6, false);
+    i2c_write_blocking(i2c_port, mpu6050_address, &val, 1, true);
+    i2c_read_blocking(i2c_port, mpu6050_address, buffer, 6, false);
 
     for (int i = 0; i < 3; i++)
     {
@@ -54,8 +58,8 @@ void mpu6050_read_raw(i2c_inst_t *I2C_PORT,int16_t accel[3], int16_t gyro[3], in
 
     // Lê temperatura a partir do registrador 0x41 (2 bytes)
     val = 0x41;
-    i2c_write_blocking(I2C_PORT, mpu6050_address, &val, 1, true);
-    i2c_read_blocking(I2C_PORT, mpu6050_address, buffer, 2, false);
+    i2c_write_blocking(i2c_port, mpu6050_address, &val, 1, true);
+    i2c_read_blocking(i2c_port, mpu6050_address, buffer, 2, false);
 
     *temp = (buffer[0] << 8) | buffer[1];
 }
--- lib/mpu6050/mpu6050.h
+++ lib/mpu6050/mpu6050.h
@@ -9,7 +9,9 @@ static int mpu6050_address = 0x68;
 void setup_I2C_mpu6050(i2c_inst_t *I2C_PORT, uint I2C_SDA, uint I2C_SCL, uint clock);
 
 // Função para resetar e inicializar o MPU6050
-void mpu6050_reset(i2c_inst_t *I2C_PORT);
+void mpu6050_reset();
+
+void mpu6050_init(i2c_inst_t *I2C_PORT);
 
 // Função para ler dados crus do acelerômetro, giroscópio e temperatura
-void mpu6050_read_raw(i2c_inst_t *I2C_PORT,int16_t accel[3], int16_t gyro[3], int16_t *temp);
+void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp);
